da87ba4f571bf3c6efe929d4f80b375ea4333d4e
Averyanov Oleg <o.averyanov@qvant.ru>
2024-09-01 19:53:09 +0300

    refactor: optimization of loading currency quotes when changing currencies

--- src/pages/MainPage.vue
+++ src/pages/MainPage.vue
@@ -1,45 +1,44 @@
 <script setup lang="ts">
-import { ref, watch } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useCurrencyStore } from '@/stores'
 import { ElNotification } from 'element-plus'
 import { Currencies } from '@/constants'
+import { generateExchangeRatesStructure } from '@/helpers'
 
 interface ITableRow {
   currency: Currencies
   value: number
 }
 
-const tableData = ref<ITableRow[]>([])
+const exchangeRate = ref(generateExchangeRatesStructure(Currencies))
 
-watch(
-  () => useCurrencyStore().selectedCurrency,
-  async () => {
-    try {
-      const exchangeRate = await useCurrencyStore().getExchangeRate()
+onMounted(async () => {
+  try {
+    exchangeRate.value = await useCurrencyStore().getExchangeRate()
+  } catch (e) {
+    ElNotification({
+      title: 'Error',
+      message: 'Что то пошло не так',
+      type: 'error'
+    })
+  }
+})
 
-      tableData.value = Object.keys(Currencies)
-        .filter((c) => c !== useCurrencyStore().selectedCurrency)
-        .reduce((acc, currency) => {
-          acc.push({
-            currency,
-            value:
-              exchangeRate[currency as Currencies][
-                useCurrencyStore().selectedCurrency
-              ]
-          })
-
-          return acc
-        }, [] as any)
-    } catch (e) {
-      ElNotification({
-        title: 'Error',
-        message: 'Что то пошло не так',
-        type: 'error'
+const tableData = computed(() => {
+  return (Object.keys(Currencies) as Currencies[])
+    .filter((c) => c !== useCurrencyStore().selectedCurrency)
+    .reduce((acc, currency: Currencies) => {
+      acc.push({
+        currency,
+        value:
+          exchangeRate.value[currency][
+            useCurrencyStore().selectedCurrency
+          ]
       })
-    }
-  },
-  { immediate: true }
-)
+
+      return acc
+    }, [] as ITableRow[])
+})
 </script>
 
 <template>
